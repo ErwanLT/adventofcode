b0f786ad98fb57c574d67e048da51746dee816ef
Erwan <erwanletutour.elt@gmail.com>
2021-11-29 10:59:33 +0100

    add 2020 days 9

---
+++ aoc-2020/src/main/java/aoc/day09/Day09.java
@@ -0,0 +1,58 @@
+package aoc.day09;
+
+import aoc.Day;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.Arrays.copyOfRange;
+import static java.util.Arrays.stream;
+
+public class Day09 implements Day {
+
+    private static long[] inputs;
+
+    @Override
+    public String part1(List<String> input) {
+        castInput(input);
+        return String.valueOf(findNumber());
+    }
+
+    private void castInput(List<String> input) {
+        inputs = input.stream().mapToLong(Long::parseLong).toArray();
+    }
+
+    @Override
+    public String part2(List<String> input) {
+        castInput(input);
+        return String.valueOf(findWeakness(findNumber()));
+    }
+
+    private static long findNumber() {
+        for (int k = 0; k < inputs.length - 25; k++) {
+            Set<Long> sums = new HashSet<>();
+            for (int i = k; i < k + 25; i++) {
+                for (int j = i + 1; j < k + 25; j++) {
+                    sums.add(inputs[i] + inputs[j]);
+                }
+            }
+            if (!sums.contains(inputs[k + 25])) {
+                return inputs[k + 25];
+            }
+        }
+        return 0;
+    }
+
+    private static long findWeakness(long invalidNumber) {
+        for (int i = 2; i < inputs.length; i++) {
+            for (int j = 0; j <= inputs.length - i; j++) {
+                if (stream(inputs, j, j + i).sum() == invalidNumber) {
+                    long[] window = copyOfRange(inputs, j, j + i + 1);
+                    return stream(window).max().getAsLong() + stream(window).min().getAsLong();
+                }
+            }
+        }
+        return 0;
+    }
+}
