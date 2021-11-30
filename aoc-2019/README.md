10a190ec7d73b58e625c9a267e5daacb2bc1d6de
Erwan <erwanletutour.elt@gmail.com>
2021-11-30 10:15:07 +0100

    2019 day 01

---
+++ aoc-2019/README.md
@@ -0,0 +1,8 @@
+# Advent of Code 2019
+
+[![About](https://img.shields.io/badge/Advent%20of%20Code-2019-brightgreen)](https://adventofcode.com/2019/about)
+[![Language](https://img.shields.io/badge/Language-Java-orange)](https://www.java.com/)
+[![Days completed](https://img.shields.io/badge/Days%20completed-11-blue)]()
+[![Stars](https://img.shields.io/badge/⭐️-19-yellow)]()
+
+* [Day 1](src/main/java/aoc/day01/README.md)
---
+++ aoc-2019/src/main/java/aoc/day01/Day01.java
@@ -0,0 +1,40 @@
+package aoc.day01;
+
+import aoc.Day;
+import aoc.ParseUtils;
+
+import java.util.List;
+
+public class Day01 implements Day {
+
+    private static int[] inputs;
+
+    @Override
+    public String part1(List<String> input) {
+        inputs = ParseUtils.castInputToIntArray(input);
+
+        int necessaryFuel = 0;
+        for (int moduleMasse: inputs) {
+            necessaryFuel += Math.floor(moduleMasse/3)-2;
+        }
+
+        return String.valueOf(necessaryFuel);
+    }
+
+    @Override
+    public String part2(List<String> input) {
+        int necessaryFuel = 0;
+        for (int number : inputs) {
+            int temp = number / 3 - 2;
+            necessaryFuel += temp;
+            while (temp != 0) {
+                temp = temp / 3 - 2;
+                if (temp <= 0) {
+                    temp = 0;
+                }
+                necessaryFuel += temp;
+            }
+        }
+        return String.valueOf(necessaryFuel);
+    }
+}
