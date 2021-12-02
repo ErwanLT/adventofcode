130726443fb7805ddf0e7478f2696be97bd687ff
Erwan <erwanletutour.elt@gmail.com>
2021-12-02 16:50:18 +0100

    2021 day code clean

--- aoc-2021/src/main/java/aoc/day02/Day02.java
+++ aoc-2021/src/main/java/aoc/day02/Day02.java
@@ -2,67 +2,59 @@ package aoc.day02;
 
 import aoc.Day;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Day02 implements Day {
-
-    private static final List<Instruction> instructions = new ArrayList<>();
 
     @Override
     public String part1(List<String> input) {
 
-        parseInput(input);
+        AtomicInteger horizontalPosition = new AtomicInteger();
+        AtomicInteger depth = new AtomicInteger();
 
-        int horizontalPosition = 0;
-        int depth = 0;
-
-        for (Instruction instruction: instructions) {
+        input.forEach( i -> {
+            String[] array = i.split(" ");
+            Instruction instruction = new Instruction(array[0], Integer.parseInt(array[1]));
             switch (instruction.getDirection()){
                 case "forward" :
-                    horizontalPosition += instruction.getValue();
+                    horizontalPosition.addAndGet(instruction.getValue());
                     break;
                 case "up" :
-                    depth -= instruction.getValue();
+                    depth.addAndGet(-instruction.getValue());
                     break;
                 case "down" :
-                    depth += instruction.getValue();
+                    depth.addAndGet(instruction.getValue());
                     break;
             }
-        }
-
-        return String.valueOf(horizontalPosition * depth);
-    }
-
-    private void parseInput(List<String> input) {
-        input.forEach( s -> {
-            String[] array = s.split(" ");
-            Instruction i = new Instruction(array[0], Integer.parseInt(array[1]));
-            instructions.add(i);
         });
+
+        return String.valueOf(horizontalPosition.get() * depth.get());
     }
 
     @Override
     public String part2(List<String> input) {
-        int horizontalPosition = 0;
-        int depth = 0;
-        int aim = 0;
+        AtomicInteger horizontalPosition = new AtomicInteger();
+        AtomicInteger depth = new AtomicInteger();
+        AtomicInteger aim = new AtomicInteger();
 
-        for (Instruction instruction: instructions) {
+        input.forEach( i -> {
+            String[] array = i.split(" ");
+            Instruction instruction = new Instruction(array[0], Integer.parseInt(array[1]));
             switch (instruction.getDirection()){
                 case "forward" :
-                    horizontalPosition += instruction.getValue();
-                    depth += (aim * instruction.getValue());
+                    horizontalPosition.addAndGet(instruction.getValue());
+                    depth.addAndGet((aim.get() * instruction.getValue()));
                     break;
                 case "up" :
-                    aim -= instruction.getValue();
+                    aim.addAndGet(-instruction.getValue());
                     break;
                 case "down" :
-                    aim += instruction.getValue();
+                    aim.addAndGet(instruction.getValue());
                     break;
             }
-        }
+        });
 
-        return String.valueOf(horizontalPosition * depth);
+        return String.valueOf(horizontalPosition.get() * depth.get());
     }
 }
