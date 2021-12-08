f3b9d335bb481a89b60f797e1ab0ecfe8ac493dc
Erwan <erwanletutour.elt@gmail.com>
2021-12-08 08:34:05 +0100

    2021 day 8 part 1

--- aoc-2021/src/main/java/aoc/day08/Day08.java
+++ aoc-2021/src/main/java/aoc/day08/Day08.java
@@ -3,11 +3,17 @@ package aoc.day08;
 import aoc.Day;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Day08 implements Day {
     @Override
     public String part1(List<String> input) {
-        return null;
+        long count = input.stream().filter(e -> !e.isBlank())
+                .map(e -> e.split("\\|")[1])
+                .flatMap(e -> Stream.of(e.split(" ")))
+                .filter(e -> e.length() == 2 || e.length() == 3 || e.length() == 4 || e.length() == 7)
+                .count();
+        return String.valueOf(count);
     }
 
     @Override
