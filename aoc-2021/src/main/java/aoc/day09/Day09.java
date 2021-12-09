d9d074ae3b0e2ff1429afea9bc5602fdd16e3613
Erwan <erwanletutour.elt@gmail.com>
2021-12-09 08:55:34 +0100

    2021 day 9 part 2

--- aoc-2021/src/main/java/aoc/day09/Day09.java
+++ aoc-2021/src/main/java/aoc/day09/Day09.java
@@ -4,11 +4,12 @@ import aoc.Day;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Day09 implements Day {
 
     private static int H, W;
-    private List<List<Integer>> data;
+    private static List<List<Integer>> data;
     private static boolean[][] visited;
     private static int[] dxArr = new int[]{1,-1,0,0};
     private static int[] dyArr = new int[]{0,0,1,-1};
@@ -46,6 +47,39 @@ public class Day09 implements Day {
 
     @Override
     public String part2(List<String> input) {
-        return null;
+
+        visited = new boolean[H][W];
+        // too lazy to change to maxHeap
+        PriorityQueue<Integer> minHeap=new PriorityQueue<>();
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                if (!visited[i][j]) {
+                    visited[i][j] = true;
+                    if (data.get(i).get(j) != 9) {
+                        int area = dfs(i, j);
+                        minHeap.add(-area);
+                    }
+                }
+            }
+        }
+        return String.valueOf(minHeap.poll() * minHeap.poll() * minHeap.poll() * -1);
     }
+
+    public static int dfs(int i, int j) {
+        int area = 1;
+        for (int k=0;k<4; k++) {
+            int dx = dxArr[k];
+            int dy = dyArr[k];
+            if (dx + i >= 0 && dx + i < H && dy + j >= 0 && dy + j < W) {
+                int ii = dx + i;
+                int jj = dy + j;
+                if (!visited[ii][jj] && data.get(ii).get(jj) < 9) {
+                    visited[ii][jj] = true;
+                    area += dfs(ii, jj);
+                }
+            }
+        }
+        return area;
+    }
+
 }
