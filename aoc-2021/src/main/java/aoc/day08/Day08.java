b916554be5b70b37e86e4b627373990a5918a903
Erwan <erwanletutour.elt@gmail.com>
2021-12-09 12:40:59 +0100

    refactor day 8

--- aoc-2021/src/main/java/aoc/day08/Day08.java
+++ aoc-2021/src/main/java/aoc/day08/Day08.java
@@ -2,21 +2,22 @@ package aoc.day08;
 
 import aoc.Day;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 import java.util.function.BiFunction;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static com.google.common.collect.ObjectArrays.concat;
-import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
 
 public class Day08 implements Day {
     @Override
     public String part1(List<String> input) {
-        return solve(input, DisplayOutputDecoder::countUniqueLengthOutputValues);
+
+        long count = input.stream()
+                .map(e -> e.split("\\|"))
+                .map(e -> new String[][]{e[0].split(" "), stream(e[1].split(" ")).filter(f -> !f.isBlank()).toArray(String[]::new)})
+                .flatMap(e -> stream(e[1]))
+                .filter(e -> e.length() == 2 || e.length() == 3 || e.length() == 4 || e.length() == 7)
+                .count();
+
+        return String.valueOf(count);
     }
 
     @Override
