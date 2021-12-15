44bccd064a3c365973ff023c723ef69376390786
Erwan <erwanletutour.elt@gmail.com>
2021-12-15 16:06:45 +0100

    add 2015 day 8

--- aoc-2015/README.md
+++ aoc-2015/README.md
@@ -13,8 +13,8 @@
 | 4     | [The Ideal Stocking Stuffer](src/main/java/aoc/day04/README.md)      |     ️️️️️️⭐️⭐       |
 | 5     | [Doesn't He Have Intern-Elves For This?](src/main/java/aoc/day05/README.md)          |    ⭐️⭐ ️️       |
 | 6     | [Probably a Fire Hazard](src/main/java/aoc/day06/README.md)           |    ⭐️⭐ ️️       |
-| 7     | [Some Assembly Required](src/main/java/aoc/day07/README.md)         |     ️️       |
-| 8     | [](src/main/java/aoc/day08/README.md)         |     ️️       |
+| 7     | [Some Assembly Required](src/main/java/aoc/day07/README.md)         |     ️️⭐️⭐       |
+| 8     | [Matchsticks](src/main/java/aoc/day08/README.md)         |     ️️⭐️⭐       |
 | 9     | [](src/main/java/aoc/day09/README.md)           |     ️️       |
 | 10    | [](src/main/java/aoc/day10/README.md)            |     ️️       |
 | 11    | [](src/main/java/aoc/day11/README.md)           |     ️️       |
---
+++ aoc-2015/src/main/java/aoc/day08/Day08.java
@@ -0,0 +1,63 @@
+package aoc.day08;
+
+import aoc.Day;
+
+import java.util.List;
+
+public class Day08 implements Day {
+
+    private static final int doubleQuoteCount = 2;
+
+    @Override
+    public String part1(List<String> input) {
+        int totalCharacterCount = input.stream().mapToInt(String::length).sum();
+        int totalCharacterCountForStringLiterals = input.stream().mapToInt(Day08::getCharacterCountPt1).sum();
+
+        return String.valueOf(totalCharacterCount - totalCharacterCountForStringLiterals);
+    }
+
+    @Override
+    public String part2(List<String> input) {
+        int totalCharacterCount = input.stream().mapToInt(String::length).sum();
+        int totalCharacterCountForStringLiterals = input.stream().mapToInt(Day08::getCharacterCountPt2).sum();
+
+        return String.valueOf(totalCharacterCountForStringLiterals - totalCharacterCount);
+    }
+
+    private static int getCharacterCountPt1(String str) {
+        int characterCount = 0;
+        int strLen = str.length();
+        int currentIndex = 0;
+        while(currentIndex < strLen) {
+            char currentCharacter = str.charAt(currentIndex);
+            if(currentCharacter == '\\' && currentIndex < strLen - 1) {
+                if(str.charAt(currentIndex + 1) == '\\' || str.charAt(currentIndex + 1) == '\"') {
+                    currentIndex += 2;
+                } else if(str.charAt(currentIndex + 1) == 'x') {
+                    currentIndex += 4;
+                }
+            } else {
+                currentIndex++;
+            }
+
+            characterCount++;
+        }
+
+        return characterCount - doubleQuoteCount;
+    }
+
+    private static int getCharacterCountPt2(String str) {
+        int characterCount = 0;
+        int strLen = str.length();
+        for(int i = 0; i < strLen; i++) {
+            char currentChar = str.charAt(i);
+            if(currentChar == '\\' || currentChar == '\"') {
+                characterCount += 2;
+            } else {
+                characterCount++;
+            }
+        }
+
+        return characterCount + doubleQuoteCount;
+    }
+}
