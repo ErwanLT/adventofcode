2b70ac936b308b0bc38fa40f05ff4531d0cd40de
Erwan <erwanletutour.elt@gmail.com>
2022-12-30 09:03:48 +0100

    Aoc 2018 day 02

--- aoc-2018/README.md
+++ aoc-2018/README.md
@@ -2,9 +2,10 @@
 
 [![About](https://img.shields.io/badge/Advent%20of%20Code-2018-brightgreen)](https://adventofcode.com/2018/about)
 [![Language](https://img.shields.io/badge/Language-Java-orange)](https://www.java.com/)
-[![Days completed](https://img.shields.io/badge/Days%20completed-01-blue)]()
-[![Stars](https://img.shields.io/badge/⭐️-02-yellow)]()
+[![Days completed](https://img.shields.io/badge/Days%20completed-02-blue)]()
+[![Stars](https://img.shields.io/badge/⭐️-04-yellow)]()
 
-| Day 	 | Name 	                                                   | Completion 	 |
-|:-----:|:---------------------------------------------------------|:------------:|
-| 1   	 | [Chronal Calibration](src/main/java/aoc/day01/README.md) |     ⭐️⭐      |
+| Day 	 | Name 	                                                           | Completion 	 |
+|:-----:|:-----------------------------------------------------------------|:------------:|
+| 1   	 | [Chronal Calibration](src/main/java/aoc/day01/README.md)         |     ⭐️⭐      |
+| 2   	 | [Inventory Management System](src/main/java/aoc/day02/README.md) |     ⭐️⭐      |
---
+++ aoc-2018/src/main/java/aoc/day02/Day02.java
@@ -0,0 +1,53 @@
+package aoc.day02;
+
+import aoc.Day;
+import aoc.parser.ParseUtils;
+import com.google.common.collect.Sets;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.Optional.empty;
+
+public class Day02 implements Day {
+    @Override
+    public String part1(List<String> input) {
+        int two = 0, three = 0;
+        String[] in = ParseUtils.castInoutToStringArray(input);
+        for(String i : in){
+            List<Integer> counts = i.chars().mapToObj(c -> (i+"_").split(Character.toString(c)).length - 1).toList();
+            if(counts.contains(2)) {
+                two++;
+            }
+            if(counts.contains(3)) {
+                three++;
+            }
+        }
+        return String.valueOf(two * three);
+    }
+
+    @Override
+    public String part2(List<String> input) {
+        var com = Sets.combinations(ParseUtils.castInputToStream(input).collect(Collectors.toSet()), 2);
+        return com.stream().map(this::compareStrings).filter(Optional::isPresent).map(Optional::get).findFirst().get();
+    }
+
+    private Optional<String> compareStrings(Set<String> str) {
+        var it = str.iterator();
+        String str1 = it.next(), str2 = it.next();
+        if (str1.length() != str2.length() || str1.equals(str2)) return empty();
+        int differences = 0;
+        int diffIndex = -1;
+        for (int i = 0; i < str1.length(); i++) {
+            if(str1.charAt(i) != str2.charAt(i)) {
+                diffIndex = i;
+                if (++differences > 1) {
+                    return empty();
+                }
+            }
+        }
+        return Optional.of(new StringBuilder(str1).deleteCharAt(diffIndex).toString());
+    }
+}
