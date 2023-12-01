ef55f4f03be3a3bf413badbc2b5273be6e2022d0
Erwan <erwanletutour.elt@gmail.com>
2023-12-01 06:50:43 +0100

    AoC 2023 day 01

--- aoc-2023/README.md
+++ aoc-2023/README.md
@@ -2,5 +2,9 @@
 
 [![About](https://img.shields.io/badge/Advent%20of%20Code-2023-brightgreen)](https://adventofcode.com/2022/about)
 [![Language](https://img.shields.io/badge/Language-Java-orange)](https://www.java.com/)
-[![Days completed](https://img.shields.io/badge/Days%20completed-00-blue)]()
-[![Stars](https://img.shields.io/badge/⭐️-00-yellow)]()
+[![Days completed](https://img.shields.io/badge/Days%20completed-01-blue)]()
+[![Stars](https://img.shields.io/badge/⭐️-002-yellow)]()
+
+| Day 	 | Name 	                                                                  | Completion 	 |
+|:-----:|:------------------------------------------------------------------------|:------------:|
+| 1   	 | [Trebuchet?!](src/main/java/aoc/day01/README.md) |     ⭐️⭐      |
--- aoc-2023/src/main/java/aoc/day01/Day01.java
+++ aoc-2023/src/main/java/aoc/day01/Day01.java
@@ -1,9 +1,14 @@
 package aoc.day01;
 
 import aoc.Day2023;
+import aoc.Pair;
+
+import java.util.Comparator;
+import java.util.Map;
 
 public class Day01 extends Day2023 {
 
+    private static final Map<String, Integer> nums = Map.of("one", 1, "two", 2, "three", 3, "four", 4, "five", 5, "six", 6, "seven", 7, "eight", 8, "nine", 9);
 
     public Day01() {
         super(1);
@@ -11,11 +16,47 @@ public class Day01 extends Day2023 {
 
     @Override
     public Object part1() {
-        return null;
+        return dayStream().map(s -> firstDigit(s).getRight() + lastDigit(s).getRight()).mapToInt(Integer::parseInt).sum();
+    }
+
+    private Pair<Integer, String> firstDigit(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                return Pair.of(i, s.charAt(i) + "");
+            }
+        }
+        throw new IllegalArgumentException("problème dans l'algo");
+    }
+
+    private Pair<Integer, String> lastDigit(String s) {
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (Character.isDigit(s.charAt(i))) {
+                return Pair.of(i, s.charAt(i) + "");
+            }
+        }
+        throw new IllegalArgumentException("problème dans l'algo");
     }
 
     @Override
     public Object part2() {
-        return null;
+        return dayStream().map(s -> firstStringDigit(s) + lastStringDigit(s)).mapToInt(Integer::parseInt).sum();
+    }
+
+    private String firstStringDigit(String s) {
+        var pair = nums.keySet().stream().map(x -> Pair.of(x, s.indexOf(x))).filter(x -> x.getRight() != -1).sorted(Comparator.comparingInt(Pair::getRight)).findFirst().orElse(Pair.of("", Integer.MAX_VALUE));
+        var firstDigit = firstDigit(s);
+        if (firstDigit.getLeft() < pair.getRight()) {
+            return firstDigit.getRight();
+        }
+        return nums.get(pair.getLeft()) + "";
+    }
+
+    private String lastStringDigit(String s) {
+        var pair = nums.keySet().stream().map(x -> Pair.of(x, s.lastIndexOf(x))).filter(x -> x.getRight() != -1).sorted((x, y) -> y.getRight() - x.getRight()).findFirst().orElse(Pair.of("", Integer.MIN_VALUE));
+        var lastDigit = lastDigit(s);
+        if (lastDigit.getLeft() > pair.getRight()) {
+            return lastDigit.getRight();
+        }
+        return nums.get(pair.getLeft()) + "";
     }
 }
