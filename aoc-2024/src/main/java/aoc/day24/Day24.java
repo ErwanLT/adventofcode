1a7457e8556620115af430d3e51d3c2e0940c4ce
Erwan <erwanletutour.elt@gmail.com>
2024-12-25 20:47:49 +0100

    aoc 2024 day 24 part 2

--- aoc-2024/src/main/java/aoc/day24/Day24.java
+++ aoc-2024/src/main/java/aoc/day24/Day24.java
@@ -4,6 +4,8 @@ import aoc.Day2024;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -75,6 +77,67 @@ public class Day24 extends Day2024 {
 
     @Override
     public Object part2() {
-        return null;
+        var input = Arrays.asList(dayStrings());
+        Map<String, String> registers = getRegisters(input);
+        List<String> swaps = new ArrayList<>();
+        int index = 0;
+        String current = "";
+        while (registers.containsKey(String.format("x%02d", index))) {
+            String x = String.format("x%02d", index);
+            String y = String.format("y%02d", index);
+            String z = String.format("z%02d", index);
+            if (index == 0) {
+                current = findExpression(registers, x, "AND", y);
+            } else {
+                String xor = findExpression(registers, x, "XOR", y);
+                String and = findExpression(registers, x, "AND", y);
+                String next = findExpression(registers, xor, "XOR", current);
+                if (next == null) {
+                    swaps.addAll(List.of(xor, and));
+                    swapRegisters(registers, xor, and);
+                    index = 0;
+                    continue;
+                }
+                if (!next.equals(z)) {
+                    swaps.addAll(List.of(next, z));
+                    swapRegisters(registers, next, z);
+                    index = 0;
+                    continue;
+                }
+                next = findExpression(registers, xor, "AND", current);
+                current = findExpression(registers, and, "OR", next);
+            }
+            index++;
+        }
+        return swaps.stream().sorted().collect(Collectors.joining(","));
+    }
+
+    private static String findExpression(Map<String, String> registers, String op1, String op, String op2) {
+        return registers.entrySet().stream()
+                .filter(entry -> {
+                    String value = entry.getValue();
+                    return value.equals(op1 + " " + op + " " + op2) || value.equals(op2 + " " + op + " " + op1);
+                })
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+    }
+
+    private static void swapRegisters(Map<String, String> registers, String register1, String register2) {
+        String temp = registers.put(register1, registers.get(register2));
+        registers.put(register2, temp);
+    }
+
+    private static Map<String, String> getRegisters(List<String> input) {
+        Map<String, String> registers = new HashMap<>();
+        for (String line : input.subList(0, input.indexOf(""))) {
+            String[] parts = line.split(": ");
+            registers.put(parts[0], parts[1]);
+        }
+        for (String line : input.subList(input.indexOf("") + 1, input.size())) {
+            String[] parts = line.split(" -> ");
+            registers.put(parts[1], parts[0]);
+        }
+        return registers;
     }
 }
