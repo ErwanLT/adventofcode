105866fad4d774a4fdbb4f814e44436ef911a54f
Erwan LE TUTOUR <erwanletutour.elt@gmail.com>
2025-12-01 08:00:37 +0100

    2025 day 1

--- aoc-2025/src/main/java/aoc/day01/Day01.java
+++ aoc-2025/src/main/java/aoc/day01/Day01.java
@@ -2,20 +2,60 @@ package aoc.day01;
 
 import aoc.Day2025;
 
+import java.util.List;
+
 public class Day01 extends Day2025 {
 
+    private record Rotation(char direction, int distance) {}
+
+    private final List<Rotation> rotations;
 
     public Day01() {
-        super(1, "Day 01");
+        super(1, "Secret Entrance");
+        this.rotations = dayStream()
+                .map(line -> new Rotation(line.charAt(0), Integer.parseInt(line.substring(1))))
+                .toList();
     }
 
     @Override
     public Object part1() {
-        return null;
+        int currentPosition = 50;
+        int zerosCount = 0;
+
+        for (Rotation rotation : rotations) {
+            if (rotation.direction == 'R') {
+                currentPosition = (currentPosition + rotation.distance) % 100;
+            } else { // 'L'
+                currentPosition = (currentPosition - rotation.distance % 100 + 100) % 100;
+            }
+
+            if (currentPosition == 0) {
+                zerosCount++;
+            }
+        }
+
+        return zerosCount;
     }
 
     @Override
     public Object part2() {
-        return null;
+        int currentPosition = 50;
+        int totalZerosCount = 0;
+
+        for (Rotation rotation : rotations) {
+            for (int i = 0; i < rotation.distance; i++) {
+                if (rotation.direction == 'R') {
+                    currentPosition = (currentPosition + 1) % 100;
+                } else { // 'L'
+                    currentPosition = (currentPosition - 1 + 100) % 100;
+                }
+
+                if (currentPosition == 0) {
+                    totalZerosCount++;
+                }
+            }
+        }
+
+        return totalZerosCount;
     }
 }
