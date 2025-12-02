37f8dd1c8b9dfebc6fd4ed50e9c3ba0c310035a3
Erwan LE TUTOUR <erwanletutour.elt@gmail.com>
2025-12-02 06:31:20 +0100

    2025 day 2

--- aoc-2025/README.md
+++ aoc-2025/README.md
@@ -2,15 +2,15 @@
 
 [![About](https://img.shields.io/badge/Advent%20of%20Code-2025-brightgreen)](https://adventofcode.com/2025/about)
 [![Language](https://img.shields.io/badge/Language-Java-orange)](https://www.java.com/)
-[![Days completed](https://img.shields.io/badge/Days%20completed-1-blue)]()
-[![Stars](https://img.shields.io/badge/⭐️-2-yellow)]()
+[![Days completed](https://img.shields.io/badge/Days%20completed-2-blue)]()
+[![Stars](https://img.shields.io/badge/⭐️-4-yellow)]()
 
 [![](img/Aoc2025.png)](https://adventofcode.com/2025)
 
 | Day 	  | Name 	                                               | Completion 	 |
 |:------:|:-----------------------------------------------------|:------------:|
 | 1   	  | [Secret Entrance](src/main/java/aoc/day01/README.md) |     ⭐️⭐      |
-| 2   	  | [TBD](src/main/java/aoc/day02/README.md)             |              |
+| 2   	  | [Gift Shop](src/main/java/aoc/day02/README.md)       |     ⭐️⭐      |
 | 3   	  | [TBD](src/main/java/aoc/day03/README.md)             |              |
 | 4   	  | [TBD](src/main/java/aoc/day04/README.md)             |              |
 | 5   	  | [TBD](src/main/java/aoc/day05/README.md)             |              |
--- aoc-2025/src/main/java/aoc/day02/Day02.java
+++ aoc-2025/src/main/java/aoc/day02/Day02.java
@@ -2,20 +2,82 @@ package aoc.day02;
 
 import aoc.Day2025;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day02 extends Day2025 {
 
+    private record Range(long start, long end) {
+    }
+
+    private final List<Range> ranges;
 
     public Day02() {
-        super(2, "Day 02");
+        super(2, "Gift Shop");
+        this.ranges = new ArrayList<>();
+        String line = day();
+        String[] rangeStrings = line.split(",");
+        for (String rangeStr : rangeStrings) {
+            String[] parts = rangeStr.split("-");
+            this.ranges.add(new Range(Long.parseLong(parts[0]), Long.parseLong(parts[1])));
+        }
     }
 
     @Override
     public Object part1() {
-        return null;
+        long totalInvalidSum = 0;
+        for (Range range : this.ranges) {
+            for (long i = range.start(); i <= range.end(); i++) {
+                if (isInvalid(i)) {
+                    totalInvalidSum += i;
+                }
+            }
+        }
+        return totalInvalidSum;
+    }
+
+    private boolean isInvalid(long number) {
+        String s = String.valueOf(number);
+        int len = s.length();
+        if (len % 2 != 0) {
+            return false;
+        }
+        int half = len / 2;
+        String firstHalf = s.substring(0, half);
+        String secondHalf = s.substring(half);
+        return firstHalf.equals(secondHalf);
     }
 
     @Override
     public Object part2() {
-        return null;
+        long totalInvalidSum = 0;
+        for (Range range : this.ranges) {
+            for (long i = range.start(); i <= range.end(); i++) {
+                if (isInvalidPart2(i)) {
+                    totalInvalidSum += i;
+                }
+            }
+        }
+        return totalInvalidSum;
+    }
+
+    private boolean isInvalidPart2(long number) {
+        String s = String.valueOf(number);
+        int len = s.length();
+        if (len < 2) { // A sequence repeated at least twice must have length >= 2
+            return false;
+        }
+
+        // The length of the repeating sub-sequence
+        for (int subLen = 1; subLen <= len / 2; subLen++) {
+            if (len % subLen == 0) { // The string must be composed of a whole number of repetitions
+                String sequence = s.substring(0, subLen);
+                if (s.equals(sequence.repeat(len / subLen))) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
